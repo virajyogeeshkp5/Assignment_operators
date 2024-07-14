fd4f4f6ae9b16f9e16d7ea43e8620fd38e437d3b
YOGEESH K P <yogeesh793@gmail.com>
2024-07-14 22:32:29 +0530

    Add files via upload

---
+++ Assignment_operators.py
@@ -0,0 +1,15 @@
+# Assignment operators
+a=10
+print("Assignment operators")
+# Addition Assignment
+a+=5
+print("a+=5:",a)
+# Subtraction Assignment
+a-=5
+print("a-=5:",a)
+# Multiplication Assignment
+a*=5
+print("a*=5:",a)
+# Division Assignment
+a/=5
+print("a/=5:",a)
